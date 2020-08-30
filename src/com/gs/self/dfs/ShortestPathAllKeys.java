089442b3d6b4d073493964a7383edd97546c69a3
gopal <gopal@192.168.10.101>
2020-08-30 01:15:09 -0400

    graph bfs dfs problems

---
+++ src/com/gs/self/dfs/ShortestPathAllKeys.java
@@ -0,0 +1,87 @@
+package com.gs.self.dfs;
+
+import java.util.*;
+
+public class ShortestPathAllKeys {
+
+public int shortestPathAllKeys(String[] grid) {
+        
+        Set<Character> expectedKeys = new HashSet<Character>();
+        Queue<int[]> queue = new LinkedList<int[]>();
+        boolean[][] visited  = new boolean[grid.length][];
+            
+        for(int row = 0; row < grid.length; row++){
+            int colLen = grid[row].length();
+            visited[row] = new boolean[colLen];
+            for(int col = 0; col < colLen; col++){
+                char c = grid[row].charAt(col);
+                if(Character.isLowerCase(c)){
+                    expectedKeys.add(c);
+                }else if(c == '@'){
+                    queue.offer(new int[]{row, col});
+                    visited[row][col] = true;
+                }
+            }
+        }
+        
+        Set<Character> collectedKeys = new HashSet<Character>();
+        
+        int steps = 0;
+        while(!queue.isEmpty()){
+            int sz = queue.size();
+            for(int i = 0; i < sz; i++){
+                int[] current = queue.poll();
+                addNeighboursToQueue(grid, queue, current, collectedKeys, visited);
+            }
+            steps++;
+            if(expectedKeys.size() == collectedKeys.size()){
+                return steps;
+            }
+            
+        }
+        
+        return (expectedKeys.size() == collectedKeys.size()) ? steps : -1;
+    }
+    
+    private void addNeighboursToQueue(String[] grid, Queue<int[]> queue, int[] current, Set<Character> collectedKeys, boolean[][] visited){
+        int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        for(int[] dir : directions){
+            addToQueue(grid, queue, current[0] + dir[0] , current[1] + dir[1], collectedKeys, visited);
+        }
+    }
+    
+    private void addToQueue(String[] grid,Queue<int[]> queue, int row , int col, Set<Character> collectedKeys, boolean[][] visited){
+       if(row < 0 || row >= grid.length || col < 0 || col >= grid[row].length() || visited[row][col]){
+	            return;
+	        }
+	        char c = grid[row].charAt(col);
+	        if(c == '#'){
+	            visited[row][col] = true;
+	        }else if(Character.isLowerCase(c)){
+	           visited[row][col] = true;
+	           queue.add(new int[]{row, col});
+	           collectedKeys.add(c);
+	        }else if(Character.isUpperCase(c) && collectedKeys.contains(Character.toLowerCase(c))){
+	        	queue.add(new int[]{row, col});
+	            visited[row][col] = true;
+	        }else{
+	          queue.add(new int[]{row, col});
+	          visited[row][col] = true;
+	        }
+	        System.err.println(collectedKeys);
+        
+    }
+	    
+	public static void main(String[] args) {
+		String[] grid = {"@.a.#",
+				         "###.#",
+				         "b.A.B"};
+		
+		//System.err.println(new ShortestPathAllKeys().shortestPathAllKeys(grid));
+		
+		grid = new String[]{"@Aa"};
+		System.err.println(new ShortestPathAllKeys().shortestPathAllKeys(grid));
+
+	}
+
+}
--- src/com/gs/self/dfsbfscombination/ShortestBridge.java
+++ src/com/gs/self/dfsbfscombination/ShortestBridge.java
@@ -1,80 +1,118 @@
 package com.gs.self.dfsbfscombination;
     
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
     
 public class ShortestBridge {
   
-      public int shortestBridge(int}]}] A) {
-    
-        Queue<int}]> queue = new LinkedList<int}]>();
-        boolean}]}] visited = new boolean}A.length]}A}0].length];
-        
-        findFirstIslandConnectedCorners(queue, A, visited);
-        int step = -1 ;
-        while (!queue.isEmpty()) {
-          int sz = queue.size();
-          System.err.println("step" + step);
-          System.err.println(Arrays.deepToString(visited));
-          for (int i = 0; i < sz; i++) {
-            int}] cell = queue.poll();
-            int row = cell}0];
-            int col = cell}1];
-            if (!visited}row]}col] && A}row]}col] == 1) {
-              return step;
-            }
-            visited}row]}col] = true;
-            addDirectionsToQueue(queue, A, row, col - 1, visited);
-            addDirectionsToQueue(queue, A, row, col + 1, visited);
-            addDirectionsToQueue(queue, A, row - 1, col, visited);
-            addDirectionsToQueue(queue, A, row + 1, col, visited);
-          }
-          step++;
-        }
-    
-        return -1;
-      }
-    
-      private void addDirectionsToQueue(Queue<int}]> queue, int}]}] A, int row, int col, boolean}]}] visited) {
-        
-        if (row < 0 || row >= A.length || col < 0 || col >= A}row].length || visited}row]}col]) {
-          return;
-        }
-        //visited}row]}col] = true;
-        queue.offer(new int}] {row, col});
-      }
-    
-      private void findFirstIslandConnectedCorners(Queue<int}]> queue, int}]}] A, boolean}]}] visited) {
-     
-        outerloop:
-        for (int row = 0; row < A.length; row++) {
-          for (int col = 0; col < A}row].length; col++) {
-            if (A}row]}col] == 1) {
-              dfs(queue, A, row, col, visited);
-              break outerloop;
-            }
-          }
-        }
-    
-      }
-    
-      private void dfs(Queue<int}]> queue, int}]}] A, int row, int col, boolean}]}] visited) {
-        if (row < 0 || row >= A.length || col < 0 || col >= A}row].length || A}row]}col] == 0 || visited}row]}col]) {
-          return;
-        }
-        visited}row]}col] = true;
-        //A}row]}col] = 2;
-        queue.offer(new int}] {row, col});
-        dfs(queue, A, row + 1, col, visited);
-        dfs(queue, A, row - 1, col, visited);
-        dfs(queue, A, row, col + 1, visited);
-        dfs(queue, A, row, col - 1, visited);
-      }
+	 private class Cell{
+	        int row;
+	        int col;
+	        public Cell(int row, int col){
+	            this.row = row;
+	            this.col = col;
+	        }
+	    }
+	    
+	    public int shortestBridge(int[][] grid) {
+	        if(grid == null || grid.length == 0) return 0;
+	        Queue<Cell> queue = new LinkedList<Cell>();
+	        boolean[][] visited = new boolean[grid.length][];
+	        for(int row = 0; row < grid.length; row++){
+	            visited[row] = new boolean[grid[row].length];
+	        }
+	        //dfs
+	        findFirstIslandCorners(grid, queue, visited);
+	        
+	        
+	        //dfs
+	        int steps = 0;
+	        while(!queue.isEmpty()){
+	            int sz = queue.size();
+	            for(int i = 0; i< sz; i++){
+	                Cell current = queue.poll();
+	                if(hasFoundSecondIsLandCorner(grid, queue, current, visited)){
+	                    return steps;
+	                }
+	            }
+	            steps++;
+	        }
+	        
+	        return -1;
+	    }
+	    
+	    private void findFirstIslandCorners(int[][] grid, Queue<Cell> queue, boolean[][] visited){
+	        for(int row = 0; row < grid.length; row++){
+	            for(int col = 0; col < grid[row].length; col++){
+	                if(grid[row][col] == 1){
+	                   dfs(grid, queue, row, col, visited);
+	                   return;
+	                }
+	            }
+	        }
+	    }
+	    
+	    private void dfs(int[][] grid, Queue<Cell> queue, int row, int col, boolean[][] visited){
+	    	if(!isValidBoundary(grid, row, col)){
+	            return;
+	        }
+
+	    	if(visited[row][col] || grid[row][col] != 1){
+	            return;
+	        }
+	        
+	        visited[row][col] = true;
+	        queue.offer(new Cell(row, col));
+	        
+	        int[][] directions = { {1,0}, {0,1}, {-1, 0}, {0, -1}};
+	        for(int[] dir : directions){
+	            dfs(grid, queue, row + dir[0], col + dir[1], visited);
+	        }
+	        
+	    }
+	   
+	    private boolean hasFoundSecondIsLandCorner(int[][] grid, Queue<Cell> queue, Cell current, boolean[][] visited){
+	        
+	    	boolean found = false;
+	    	
+	    	int[][] directions = { {1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+	        for(int[] direction : directions){
+	            found = found || bfshelper(grid, queue, current.row + direction[0] ,  current.col + direction[1], visited);
+	        }
+	        return found;
+	    }
+	    
+	    private boolean bfshelper(int[][] grid, Queue<Cell> queue, int row, int col, boolean[][] visited){
+	        if(!isValidBoundary(grid, row, col)){
+	            return false;
+	        }
+	        
+	        if(visited[row][col]) {
+	            return false;
+	        }
+	        
+	        if(grid[row][col] == 1){
+	            return true;
+	        }
+	        
+	        visited[row][col] = true;
+	        queue.offer(new Cell(row, col));
+	        
+	        return false;
+	        
+	    }
+	    
+	    private boolean isValidBoundary(int[][] grid, int row, int col){
+	        if(row >= 0 && row < grid.length && col >= 0 && col < grid[row].length)
+	            return true;
+	        
+	        return false;
+	    }
       
-      public static void main(String}] args) {
-        int}]}] A = {
+      public static void main(String[] args) {
+        int[][] A = {
                     {0, 1},
+                    {0, 0},
                     {1, 0}
                     };
         
