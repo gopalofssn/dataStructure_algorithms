553f94c410d19f64a767ecd81e642af892a18cc5
gopal <gopal@192.168.10.101>
2020-09-04 19:06:26 -0400

    update

--- src/com/gs/binarysearchtree/DeleteNodeInBST.java
+++ src/com/gs/binarysearchtree/DeleteNodeInBST.java
@@ -51,5 +51,7 @@ public class DeleteNodeInBST {
 			System.out.println("After Delete  - " + root);
 			
 			System.out.println("After Delete  - " + new DeleteNodeInBST().deleteNode(new TreeNode(1), 1));
+			//[2,0,33,null,1,25,40,null,null,11,31,34,45,10,18,29,32,null,36,43,46,4,null,12,24,26,30,null,null,35,39,42,44,null,48,3,9,null,14,22,null,null,27,null,null,null,null,38,null,41,null,null,null,47,49,null,null,5,null,13,15,21,23,null,28,37,null,null,null,null,null,null,null,null,8,null,null,null,17,19,null,null,null,null,null,null,null,7,null,16,null,null,20,6]
+			//33
 		}
 }
