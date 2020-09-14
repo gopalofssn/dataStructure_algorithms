93388fa10f5ad9f853262a5f69dd813d46083310
gopal <gopal@192.168.10.100>
2020-09-14 00:06:44 -0400

    super program

--- challenging/sequence/TPatternS1S2S3ToSnIncoming.java
+++ challenging/sequence/TPatternS1S2S3ToSnIncoming.java
@@ -1,23 +1,69 @@
 package sequence;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class TPatternS1S2S3ToSnIncoming {
+	
+	private ArrayList<Integer>[] charIndexHolder;
+	
+	public TPatternS1S2S3ToSnIncoming(String t) {
+		charIndexHolder = new ArrayList[26];
+		for(int i = 0; i < 26; i++) {
+			charIndexHolder[i] = new ArrayList<Integer>();
+		}
+		buildGrid(t);
+	}
+	
+	private void buildGrid(String t) {
+		for(int i = 0; i < t.length(); i++) {
+			char c = t.charAt(i);
+			int index = c - 'a';
+			charIndexHolder[index].add(i);
+		}
+		System.out.println(" charIndexHolder ");
+	    for(char c = 'a' ; c <='z'; c++) {
+	    	int index = c - 'a';
+	    	System.out.println(c + "'s postions - > " + charIndexHolder[index]);
+	    }
+	}
+
+	public boolean isSubSequence(String s) {
+		if(s == null || s.isEmpty() ) return true;
+		int key = 0;
+		for(char c : s.toCharArray()) {
+			int index = c - 'a';
+			key = binarySearch(charIndexHolder[index], key);
+			if(key == -1) return false;
+			key++;
+		}
+		return true;
+	}
+     // [0, 2, 5]
+	private int binarySearch(ArrayList<Integer> list, int target) {
+		int left = 0;
+		int right = list.size() -1;
+		while(left <= right) {
+			int mid = left + ((right - left) / 2);
+			if(mid == 0 && list.get(mid) >= target) {
+				return list.get(mid);
+			}else if(mid > 0 && list.get(mid -1) < target  && list.get(mid) >= target){
+				return list.get(mid);
+			}else if(target > list.get(mid) ) {
+				left = mid + 1;
+			}else {
+				right = mid - 1;
+			}	
+		}
+		System.out.println("oh ha");
+		return -1;
+	}
+
 	public static void main(String[] args) {
 		
 		String t = "hghgghacbdefuaz";
-		ArrayList<Integer>[] idx = new ArrayList[26]; // Just for clarity
-        for (int i = 0; i < t.length(); i++) {
-        	char c = t.charAt(i);
-        	int row = c - 'a';
-            if (idx[row] == null)
-                idx[row] = new ArrayList<>();
-            idx[row].add(i);
-        }
+		TPatternS1S2S3ToSnIncoming c = new TPatternS1S2S3ToSnIncoming(t);
+        String s = "aeazh";
+        System.out.println("isSubSequence ? " + c.isSubSequence(s));
         
-        for(List<Integer> i : idx) {
-        	System.out.println(i);
-        }
 	}
 }
