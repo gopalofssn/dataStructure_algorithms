f39540f92544ba90ba51bf14074c8a3d7bce6024
gopal <gopal@192.168.10.100>
2020-09-27 22:23:56 -0400

    bunch of checkin

--- recursion/com/self/gs/recurssion/TargetSum494.java
+++ recursion/com/self/gs/recurssion/TargetSum494.java
@@ -3,6 +3,8 @@ package com.self.gs.recurssion;
 import java.util.*;
 
 /*
+ * 
+ * either use + or - of current num
  Input: nums is [1, 1, 1, 1, 1], S is 3. 
 Output: 5
 Explanation: 
@@ -12,8 +14,38 @@ Explanation:
 +1+1-1+1+1 = 3
 +1+1+1-1+1 = 3
 +1+1+1+1-1 = 3
+There are 5 ways to assign symbols to make the sum of nums be target 3.  
 
-There are 5 ways to assign symbols to make the sum of nums be target 3.   
+ approach 
+   DFS recursion
+   ------
+    2 power n time complixity
+    countAtIndex =  targetSumHelper(nums, S, index + 1, currentSum - nums[index], cache) + 
+                            targetSumHelper(nums, S, index + 1, currentSum + nums[index], cache)
+    space : O(n) , depth of nums                      
+       
+    
+    DFS recursion with memoization
+   ------
+   
+    dynamic programming
+    -------
+    
+     ?
+     
+     
+     4 3 7 2 1
+     -4-3+7 + 2 + 1 = 3
+     4 + 3 - 7 + 2 + 1 = 3
+     - 4 + 3 +7 -2-1 = 3
+     
+         -7 -4 -3 -2 -1 1 2 3 4 7
+      0  F  F  F  F  F  F F F F T
+      1  F  F  F  F  F  F F F F F
+      2
+      3
+  
+    
  */
 public class TargetSum494 {
 
@@ -41,7 +73,7 @@ public class TargetSum494 {
     }
     
 	public static void main(String[] args) {
-		int[] nums = {1, 1, 1, 1, 1};
+		int[] nums = {4, 7, 3, 2, 1};
 		int sum = 3;
 		System.out.println(new TargetSum494().findTargetSumWays(nums, sum));
 	}
